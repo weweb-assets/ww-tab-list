6318f69f2e5f9078577e37e18e919ab84f8e2b37
luka-weweb <luka@weweb.io>
2024-11-27 18:48:18 +0100

    add items to repeat

--- ww-config.js
+++ ww-config.js
@@ -18,8 +18,29 @@ export default {
     },
     properties: {
         tabTriggerElements: {
-            hidden: true,
+            label: {
+                en: 'Items',
+                fr: 'Items',
+            },
+            type: 'Info',
+            options: {
+                text: { en: 'Elements to repeat' },
+            },
+            bindable: 'repeatable',
             defaultValue: [],
+            /* wwEditor:start */
+            bindingValidation: {
+                validations: [
+                    {
+                        type: 'array',
+                    },
+                    {
+                        type: 'object',
+                    },
+                ],
+                tooltip:
+                    'A collection or an array of data: \n\n`myCollection` or `[{}, {}, ...] || ["string1", "string2", ...] || [1, 2, ...]`',
+            },
         },
     },
 };
